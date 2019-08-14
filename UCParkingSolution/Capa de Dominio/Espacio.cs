a1c01df5a7117e731f073294daa6291b1a39195f
sebastian <juan.1701514210@ucaldas.edu.co>
2019-08-14 05:23:34 -0500

    creando la clase en el proyecto de espacio
    solo se dejo pero tambien tiene errores

---
+++ UCParkingSolution/Capa de Dominio/Espacio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capa_de_Dominio
+{
+
+
+    public class Espacio
+    {
+        private int tamañoM2;
+        private String identificador;
+        private String ubicación;
+        private boolean tipoEspecial;
+        private boolean espacioLibre;
+        private String descripción;
+        public void OcuparEspacio(String tamaño, boolean TipoEspecial)
+        {
+        }
+        public boolean EspacioDisponible()
+        {
+        }
+        public boolean VerificarEspacio(String identificador)
+        {
+        }
+        public int getTamaño()
+        {
+        }
+        public String getIdentificador()
+        {
+        }
+        public String getUbicación()
+        {
+        }
+        public boolean getTipoEspacioLibre()
+        {
+        }
+        public String getDescripción()
+        {
+        }
+    }
+
+}
